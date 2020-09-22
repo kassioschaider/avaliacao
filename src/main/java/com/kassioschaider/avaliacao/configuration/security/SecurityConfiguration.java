91a9a5e0c76dd6c405773bef6f487abfe5ccfd5b
Kassio Schaider <kassio.schaider@hotmail.com>
2020-09-22 16:58:10 -0300

    Ajuste nos acessos da API por token.

--- src/main/java/com/kassioschaider/avaliacao/configuration/security/SecurityConfiguration.java
+++ src/main/java/com/kassioschaider/avaliacao/configuration/security/SecurityConfiguration.java
@@ -42,10 +42,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/api/marcas").permitAll()
-                .antMatchers(HttpMethod.GET, "/api/patrimonios").permitAll()
-                .antMatchers(HttpMethod.GET, "/api/marcas/*").permitAll()
-                .antMatchers(HttpMethod.GET, "/api//patrimonios/*").permitAll()
                 .antMatchers(HttpMethod.POST, "/auth").permitAll()
                 .anyRequest().authenticated()
                 .and().csrf().disable()
